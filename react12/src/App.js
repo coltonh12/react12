f6b33df032bd8b7f3765291278b076f4a96ddea8
Colton Hye <hyercolt@msu.edu>
2024-03-30 10:21:23 -0400

    lol

--- react12/src/App.js
+++ react12/src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+const book = {
+  title: 'harry potter and the chamber of secrets',
+  author: 'J.k Rowling',
+  published: '2007',
+  image: 'https://upload.wikimedia.org/wikipedia/en/5/5c/Harry_Potter_and_the_Chamber_of_Secrets.jpg',
+  width: '264',
+  height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img
+      className="bookCover"
+      src={book.image}
+      alt={book.title + 'cover'}
+      style={{
+        width: book.width,
+        height: book.height
+      }}
+      />
+    </div>
+  );
+}
+
+function MagicButton() {
+  return (
+    <div>
+      <h3>This is a magic button</h3>
+    <button>Magic</button>
+    </div>
+    
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Bookshelf />
       </header>
     </div>
   );
